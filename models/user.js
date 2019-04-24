8101d188c5740546c03b7ddea58f5a09289136b3
Greg Fruin <gfruin@gmail.com>
2019-04-24 16:45:59 -0500

    added schema and model to user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 //require mongoose
-
-//define commentSchema
-
+const mongoose = require('mongoose')
+//define userSchema
+const userSchema = new mongoose.Schema({
+	username: String,
+	password: String,
+	posts: []
+})
 //define the model 
-
+const User = mongoose.model('User', userSchema);
 //export module
+module.exports = User;
