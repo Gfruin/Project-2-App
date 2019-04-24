f5bdd36b63abfefe4f281103cea5932fba6df363
Greg Fruin <gfruin@gmail.com>
2019-04-24 16:37:14 -0500

    finished comment model

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,25 @@
 //require mongoose
-
+const mongoose = require('mongoose')
 //define commentSchema
+const commentSchema = new mongoose.Schema({
+	text: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	},
 
+	text: {
+		type: String,
+		required: true
+	},
+	user: {type: mongoose.Schema.Types.ObjectId, ref: "User"} //type will refer
+	//to mongoose to find the user id to print on the page.... 
+	//ref is to model of 'user'
+})
 //define the model 
-
+const Comment = new mongoose.model('Comment', commentSchema)
 //export module
+module.exports = Comment;
