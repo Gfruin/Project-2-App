88f8aef3f5fa5c1ff1f45bd8a1145dd99411d338
Greg Fruin <gfruin@gmail.com>
2019-04-24 15:49:45 -0500

    added database mongoose connections

--- db/db.js
+++ db/db.js
@@ -1,12 +1,24 @@
 //require mongoose 
+const mongoose = require('mongoose');
 
 //make connection string
-
+const connectionString = 'mongodb://localhost/tix10'
 
 //connect mongoose
-
+mongoose.connect(connectionString, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+})
 //connection.on connect
-
+mongoose.connection.on('connected', () => {
+	console.log('mongoose connected to ', connectionString);
+})
 //connection.on disconnect
-
+mongoose.connection.on('disconnected', () => {
+	console.log('mongoose disconnected to ', connectionString);
+})
 //connection.on error
+mongoose.connection.on('error', (error) => {
+	console.log('mongoose hit an error', error);
+})
