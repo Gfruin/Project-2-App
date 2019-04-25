781b47e8497027d1d3bedabcd6d9d579f327ed1b
Greg Fruin <gfruin@gmail.com>
2019-04-25 03:46:51 -0500

    added basic index route to postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,13 @@
 // require express, router, and models
-
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/post'); //may need to change the variable name and multiples
+const User = require('../models/user');
 //index route
-
+router.get('/', (req,res) => {
+	console.log('<-------------Hit the index route');
+	res.render('/index.ejs', 'Look at terminal')
+})
 //show route
 
 //new route
